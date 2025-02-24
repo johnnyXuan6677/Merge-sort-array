e0090e134d59df615904d42a41aced7a64d4c094
johnnyXuan6677 <56768834+johnnyXuan6677@users.noreply.github.com>
2025-02-24 23:15:44 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+  for(int i=0;i<n;i++){
+            nums1[m+i]=nums2[i];
+        }
+        int temp;
+        boolean condition=false;
+        int check=0;
+    while(!condition){
+        check=0;
+        for(int i=0;i<n+m-1;i++){
+            if(nums1[i]>nums1[i+1]){
+                temp=nums1[i];
+                nums1[i]=nums1[i+1];
+                nums1[i+1]=temp;
+            }else{
+                check++;
+                System.out.println(check);
+            }
+            
+        }
+       if(check==n+m-1)
+        condition=true;
+    } 
+    }
+}
